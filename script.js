4269bcd6f5fd206230a5e91211b826ef7a269c63
Yashtyagi1504 <166196060+Yashtyagi1504@users.noreply.github.com>
2025-02-27 01:51:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+let line = document.querySelector("#line");
+let degree = 0;
+
+function linee() {
+	setInterval(()=>{
+		line.style.transform = `rotate(${degree}deg)`;
+		degree += 2;
+	},20)
+}
+
+linee();
